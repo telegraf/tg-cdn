02a958400599fb7dbb33e1891546f2633c1d326f
Vitaly Domnikov <dotcypress@gmail.com>
2017-10-08 16:20:57 -0700

    update mime usage

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const fileLinks = new LRU({ max: 1000, maxAge: 1000 * 60 * 45 })
 module.exports = async function (req, res) {
   const fileId = req.url.slice(1)
   assert(fileId, 404)
-  const fileInfo = fileInfos.get(fileId) || await tg.getFile(fileId).catch(() => {})
+  const fileInfo = fileInfos.get(fileId) || await tg.getFile(fileId).catch(() => null)
   assert(fileInfo, 404)
   fileInfos.set(fileId, fileInfo)
   const link = fileLinks.get(fileId) || await tg.getFileLink(fileInfo)
   assert(link, 404)
   fileLinks.set(fileId, link)
   res.setHeader('cache-control', 'public, max-age=31536000')
-  res.setHeader('content-type', mime.lookup(fileInfo.file_path))
+  res.setHeader('content-type', mime.getType(fileInfo.file_path))
   hyperquest(link).pipe(res)
 }
 
